a3a549b0d9e103a44e0b757540cd979487058990
ivanportales <gidsp.snf19@uea.edu.br>
2019-12-18 17:17:49 -0300

    Os itens da lista agora são mostrados em um card , pra melhor diferenciar cada um

---
+++ lib/widgets/CustomAppBar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:listadecompras/blocs/BlocProdutoList.dart';
+import 'package:provider/provider.dart';
+
+class CustomAppBar extends StatelessWidget {
+
+  bool isSearching = false;
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    BlocProdutoList bloc = Provider.of<BlocProdutoList>(context);
+  // n vai dar certo com o Provider, pensando em mudar, de novo, pra Bloc com Streams
+    return AppBar(
+      leading: Icon(Icons.assignment),
+      title: Consumer<BlocProdutoList>(
+        builder: (context,produtos,_){
+          if(isSearching){
+            return TextField(
+              controller: controller,
+            );
+          }else{
+            return Text("Total: ${produtos.getTotal()}");
+          }
+        },
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: (){
+            isSearching = true;
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: (){
+            bloc.clearAll();
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/ProdutoListTile.dart
+++ lib/widgets/ProdutoListTile.dart
@@ -16,114 +16,117 @@ class ProdutoListTile extends StatelessWidget {
 
     BlocProdutoList produtos = Provider.of<BlocProdutoList>(context);
     
-    return ListTile(
-      leading: Checkbox(
-      checkColor: Colors.redAccent,
-      activeColor: Colors.black,
-      value: produto.comprado,
-      onChanged: (isChecked){
-        produtos.changeStatus(index);
-        if(isChecked){
-          produtos.incrementTotal(produto.getTotal());
-        }
-        else{
-          produtos.decrementTotal(produto.getTotal());
-        }
-      },
-      ),
-      title: Text(
-        "${produto.nome}",
-        style: (produto.comprado) ?  TextStyle(color: Colors.grey, decoration: TextDecoration.lineThrough,fontStyle: FontStyle.italic) : TextStyle()
-      ),
-      trailing: IconButton(
-        icon: Icon(Icons.edit),
-        onPressed: (){
-          showDialog(
-            barrierDismissible: false,
-            context: context,
-            builder: (_){
-              BlocTileDialog dialogBloc = BlocTileDialog(quantidadeCache:produto.quantidade,precoCache: produto.preco);
-              dialogBloc.updateTotal(produto.getTotal());
-              return AlertDialog(
-                title: Text("Produto: ${produto.nome}"),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children:<Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(
-                      icon: Icon(Icons.plus_one),
-                        border: UnderlineInputBorder(),
-                        hintText: "${produto.quantidade}",
+    return Card(
+      color: Colors.transparent,
+      child: ListTile(
+        leading: Checkbox(
+        checkColor: Colors.redAccent,
+        activeColor: Colors.black,
+        value: produto.comprado,
+        onChanged: (isChecked){
+          produtos.changeStatus(index);
+          if(isChecked){
+            produtos.incrementTotal(produto.getTotal());
+          }
+          else{
+            produtos.decrementTotal(produto.getTotal());
+          }
+        },
+        ),
+        title: Text(
+          "${produto.nome}",
+          style: (produto.comprado) ?  TextStyle(color: Colors.grey, decoration: TextDecoration.lineThrough,fontStyle: FontStyle.italic) : TextStyle()
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.edit),
+          onPressed: (){
+            showDialog(
+              barrierDismissible: false,
+              context: context,
+              builder: (_){
+                BlocTileDialog dialogBloc = BlocTileDialog(quantidadeCache:produto.quantidade,precoCache: produto.preco);
+                dialogBloc.updateTotal(produto.getTotal());
+                return AlertDialog(
+                  title: Text("Produto: ${produto.nome}"),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children:<Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                        icon: Icon(Icons.plus_one),
+                          border: UnderlineInputBorder(),
+                          hintText: "${produto.quantidade}",
+                        ),
+                        keyboardType: TextInputType.number,
+                        controller: dialogBloc.quantidadeController,
+                        onChanged: (value){
+                          if(value.isEmpty){
+                            produto.quantidade = dialogBloc.quantidadeCache;
+                          }else{
+                            produto.quantidade = int.parse(value);
+                          }
+                          dialogBloc.updateTotal(produto.getTotal());
+                        },
                       ),
-                      keyboardType: TextInputType.number,
-                      controller: dialogBloc.quantidadeController,
-                      onChanged: (value){
-                        if(value.isEmpty){
-                          produto.quantidade = dialogBloc.quantidadeCache;
-                        }else{
-                          produto.quantidade = int.parse(value);
-                        }
-                        dialogBloc.updateTotal(produto.getTotal());
-                      },
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                      icon: Icon(Icons.monetization_on),
-                        border: UnderlineInputBorder(),
-                        hintText: "${produto.preco}",
+                      TextFormField(
+                        decoration: InputDecoration(
+                        icon: Icon(Icons.monetization_on),
+                          border: UnderlineInputBorder(),
+                          hintText: "${produto.preco}",
+                        ),
+                        keyboardType: TextInputType.number,
+                        controller: dialogBloc.precoController,
+                        onChanged: (value){
+                          if(value.isEmpty){
+                            produto.preco = dialogBloc.precoCache;
+                          }else{
+                            produto.preco = double.parse(value);
+                          }
+                          dialogBloc.updateTotal(produto.getTotal());
+                        },
                       ),
-                      keyboardType: TextInputType.number,
-                      controller: dialogBloc.precoController,
-                      onChanged: (value){
-                        if(value.isEmpty){
-                          produto.preco = dialogBloc.precoCache;
-                        }else{
-                          produto.preco = double.parse(value);
-                        }
-                        dialogBloc.updateTotal(produto.getTotal());
-                      },
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 8),
-                      child: Text("${dialogBloc.total.toString()}",textAlign: TextAlign.center),
-                    ),
-                  ],
-                ),
-                actions: <Widget>[
-                  FlatButton(
-                    child: Text("Cancelar"),
-                    onPressed: (){
-                      produto.quantidade = dialogBloc.quantidadeCache;
-                      produto.preco = dialogBloc.precoCache;
-                      Navigator.pop(context);
-                    },
+                      Padding(
+                        padding: EdgeInsets.only(top: 8),
+                        child: Text("${dialogBloc.total.toString()}",textAlign: TextAlign.center),
+                      ),
+                    ],
                   ),
-                  FlatButton(
-                    child: Text("Ok"),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("Cancelar"),
                       onPressed: (){
-                        if(!produto.comprado){
-                          produto.comprado = true;
-                          produtos.update(produto);
-                          produtos.incrementTotal(produto.getTotal());
-                        }else{
-                          produto.quantidade = dialogBloc.quantidadeCache;
-                          produto.preco = dialogBloc.precoCache;
-                        }
+                        produto.quantidade = dialogBloc.quantidadeCache;
+                        produto.preco = dialogBloc.precoCache;
                         Navigator.pop(context);
                       },
                     ),
-                  ],
-                );
-              }
-            );
-          },
-        ),
-      onLongPress: (){
-        produtos.remove(produto);
-        if(produto.comprado){
-          produtos.decrementTotal(produto.getTotal());
-        }
-      },
+                    FlatButton(
+                      child: Text("Ok"),
+                        onPressed: (){
+                          if(!produto.comprado){
+                            produto.comprado = true;
+                            produtos.update(produto);
+                            produtos.incrementTotal(produto.getTotal());
+                          }else{
+                            produto.quantidade = dialogBloc.quantidadeCache;
+                            produto.preco = dialogBloc.precoCache;
+                          }
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  );
+                }
+              );
+            },
+          ),
+        onLongPress: (){
+          produtos.remove(produto);
+          if(produto.comprado){
+            produtos.decrementTotal(produto.getTotal());
+          }
+        },
+      ),
     ); 
   }
 }
